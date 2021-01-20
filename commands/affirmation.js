6885b08907ed9a296e0336f1b872f17fc4f0b204
Mickey <mickey.zheng@yahoo.com>
2021-01-20 16:08:10 -0500

    Addded !affirmation command

---
+++ commands/affirmation.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+
+// Replies with an affirmation to the user
+module.exports = {
+    name: 'affirmation',
+    description: 'Affirmation!',
+    execute(msg, args) {
+        axios.get('https://www.affirmations.dev')
+            .then(res => {
+                let affirmation = res.data.affirmation;
+                msg.channel.send(`${msg.author} ${affirmation}`);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },
+};
