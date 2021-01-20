fbc2fd892121f7607f328cf80d7ac335a0734fd3
Mickey <mickey.zheng@yahoo.com>
2021-01-20 16:58:21 -0500

    Added !meme command

---
+++ commands/meme.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const Discord = require('discord.js');
+
+// Replies with a random meme from redit
+module.exports = {
+    name: 'meme',
+    description: 'Sends back a random meme',
+    execute(msg, args) {
+        axios.get('https://meme-api.herokuapp.com/gimme')
+            .then(res => {
+                console.log(res);
+                let embed = new Discord.MessageEmbed()
+                    .setImage(`${res.data.preview[res.data.preview.length - 1]}`)
+                    .setURL(`${res.data.postLink}`)
+                    .setDescription(`Title: ${res.data.title}`)
+
+                return msg.reply(embed);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    },
+};
